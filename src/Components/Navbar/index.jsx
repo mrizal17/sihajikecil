bb2dee22989939dcbd8f87c25a6a96bc7ca5be99
Muhammad Rizal H <muhammadrizalh45@gmail.com>
2025-07-02 09:23:48 +0700

    navbar update

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -10,16 +10,19 @@ const Navbar = () => {
   return (
     <>
       {/* Tampilkan hanya di tampilan mobile */}
-      <header className="bg-[#B6F500] text-black shadow-md px-4 py-3">
+      <header className="bg-[#212121] text-black shadow-md px-4 py-3">
         <div className="flex items-center justify-between max-w-7xl mx-auto">
           {/* Logo */}
           <div className="flex items-center gap-4">
-            <img src="/images/logo.png" alt="Logo" className="w-12 h-14" />
+            <img src="/images/logo.png" alt="Logo" className="w-12 h-14 rounded-full" />
+            <div className="text-white">
             <p className="text-xl font-bold">SiHajiKecil</p>
+            <p className="text-xs">(Sistem Informasi HAJI Puskesmas Cilandak)</p>
+            </div>
           </div>
 
           {/* Menu Desktop */}
-          <nav className="hidden md:flex gap-6 items-center font-medium">
+          <nav className="hidden md:flex gap-6 items-center font-medium text-white">
             <Link to="/e-istitaah" className="hover:underline">e-Istitaah</Link>
             <Link to="/surat-vaksin" className="hover:underline">Surat Keterangan Vaksin</Link>
             <Link to="/hasil-kebugaran" className="hover:underline">Hasil Pengukuran Kebugaran</Link>
@@ -28,7 +31,7 @@ const Navbar = () => {
           </nav>
 
           {/* Hamburger Menu (Tombol) */}
-          <div className="md:hidden">
+          <div className="md:hidden text-white">
             <button onClick={toggleMenu}>
               {menuOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
             </button>
@@ -37,7 +40,7 @@ const Navbar = () => {
 
         {/* Mobile Menu (Tampil hanya di Mobile) */}
         {menuOpen && (
-          <div className="flex flex-col gap-3 mt-3 font-medium px-2 md:hidden transition-all duration-300 ease-in-out">
+          <div className="flex flex-col gap-3 mt-3 font-medium px-2 md:hidden transition-all duration-300 ease-in-out text-white">
             <Link to="/e-istitaah" onClick={toggleMenu} className="hover:underline">
               e-Istitaah
             </Link>
